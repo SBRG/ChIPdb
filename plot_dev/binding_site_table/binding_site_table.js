9d248cbe22ec25a62c59d621fe562b1295b40c3c
Katherine Decker <54454225+KatherineDecker@users.noreply.github.com>
2021-05-03 10:48:14 -0700

    clean up of inital organization complete

--- plot_dev/binding_site_table/binding_site_table.js
+++ plot_dev/binding_site_table/binding_site_table.js
@@ -1,10 +1,10 @@
 /**
  * @summary Creates interactive binding site tables for ChIPdb
- * @author Tahani Al Bulushi 
+ * @author Tahani Al Bulushi
  * requires tabulator.info
  */
- //write function to container
- function genBindingSiteTable(jsonContent,container) {
+//write function to container
+function genBindingSiteTable(jsonContent, container, target_div, igv_options) {
 
     // convert rows into objects
     var tabledata = []
@@ -12,85 +12,98 @@
         var obj = jsonContent[i];
         tabledata.push(obj) //adds items to an object
     }
-    
+
     for (let i = 0; i < tabledata.length; i++) {
         const index = tabledata[i].index;
         const bindCond = tabledata[i].condition;
         const bindStart = tabledata[i].binding_peak_start;
         const bindEnd = tabledata[i].binding_peak_end;
-        const peakStrength =tabledata[i].binding_peak_strength;
+        const peakStrength = tabledata[i].binding_peak_strength;
         const genes = tabledata[i].target_genes;
-
-        console.log(index,bindCond,bindStart,bindEnd,peakStrength,genes);
     }
-    
+
     //make a header object for the columns 
     var headerMenu = [
         {
-            label:"<i class='fas fa-arrow-right'></i> Move Column to End",
-            action:function(e, column){
+            label: "<i class='fas fa-arrow-right'></i> Move Column to End",
+            action: function (e, column) {
                 column.move("end");
             }
         }, {
-            label:"<i class='fas fa-trash'></i> Hide Column",
-            action:function(e, column){
+            label: "<i class='fas fa-trash'></i> Hide Column",
+            action: function (e, column) {
                 column.hide();
             }
         }
     ]
-    
+
     // columns object: basic info
     var columns = [{
-                        title: "TF-#",
-                        field: "index",
-                        sorter: "string",
-                    },
-                    {
-                        title: " Binding Condition",
-                        field: "condition",
-                        sorter: "string",
-                        hozAlign: "left",
-                        formatter:"money",
-                        formatterParams:{precision:4},
-                        headerContextMenu:headerMenu
-                    },
-                    {
-                        title: "Peak Location",
-                        field: "binding_peak_start",
-                        sorter: "number",
-                        hozAlign: "center",
-                        headerContextMenu:headerMenu
-                    },
-                    {
-                        title: "Peak Location",
-                        field: "binding_peak_end",
-                        sorter: "number",
-                        hozAlign: "center",
-                        headerContextMenu:headerMenu
-                    },
-                    {
-                        title: "Peak Intensity",
-                        field: "binding_peak_strength",
-                        sorter: "number",
-                        hozAlign: "center"
-                    },
-                     {
-                        title: "Target genes",
-                        field: "target_genes",
-                        sorter: "string",
-                        hozAlign: "left"
-                    },
-                ]
-    
-    var table = new Tabulator("#"+container, {
+        title: "TF-#",
+        field: "index",
+        sorter: "string",
+    },
+        {
+            title: " Binding Condition",
+            field: "condition",
+            sorter: "string",
+            hozAlign: "left",
+            formatter: "money",
+            formatterParams: {precision: 4},
+            headerContextMenu: headerMenu
+        },
+        {
+            title: "Peak Location",
+            field: "binding_peak_start",
+            sorter: "number",
+            hozAlign: "center",
+            headerContextMenu: headerMenu
+        },
+        {
+            title: "Peak Location",
+            field: "binding_peak_end",
+            sorter: "number",
+            hozAlign: "center",
+            headerContextMenu: headerMenu
+        },
+        {
+            title: "Peak Intensity",
+            field: "binding_peak_strength",
+            sorter: "number",
+            hozAlign: "center"
+        },
+        {
+            title: "Target genes",
+            field: "target_genes",
+            sorter: "string",
+            hozAlign: "left"
+        },
+    ]
+
+    var table = new Tabulator("#" + container, {
         maxHeight: "100%",
         data: tabledata,
-        columns:columns,
+        columns: columns,
         initialSort: [
             {column: "binding_condition", dir: "desc"}
         ],
         tooltips: true,
-        movableColumns:true,
-    }); 
+        movableColumns: true,
+
+        rowClick: function (e, row) { //link to the page in a database
+            var rnd = row.getData().binding_peak_start;
+            igv.removeAllBrowsers()
+            igv.createBrowser(target_div, igv_options)
+                .then(function (browser) {
+                    browser.search('gi|49175990|ref|NC_000913.2|:'.concat(numberWithCommas(rnd - 100), '-', numberWithCommas(rnd + 100)));
+                    console.log("Created IGV browser 2");
+                });
+            // document.getElementById('tb').value = rnd;
+        },
+        tooltips: function (cell) {
+            return "Click to view gene dashboard";
+        }
+    });
+    table.redraw();
 
 } 
