5ca3ef54b5c7799fa0aa4316a28b0c170ed94c42
Katherine Decker <54454225+KatherineDecker@users.noreply.github.com>
2021-06-03 22:59:39 -0700

    fix dashboard colors & method to pull bw files for tracks

--- js/igv_and_table.js
+++ js/igv_and_table.js
@@ -10,194 +10,211 @@ function generateIGVandTable(TF_name, row) {
     document.getElementById("download-bw").setAttribute("href",
         "/ChIPdb/data/e_coli/NC_000913_3/bw/compressed/" + TF_name + "_bw.zip");
 
-        // IGV
-        var igvDiv = document.getElementById("igv-div");
-        var options =
-            {
-                reference:
-                    {
-                        "id": "MG1655",
-                        "name": "E. coli MG1655",
-                        "fastaURL": "/ChIPdb/data/e_coli/NC_000913_3/sequence/sequence.fasta",
-                        "indexURL": "/ChIPdb/data/e_coli/NC_000913_3/sequence/sequence.fasta.fai",
-                        "tracks": [
-                            {
-                                "type": "annotation",
-                                "format": "gff",
-                                "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/genes.gff",
-                                "name": "Genes",
-                                "gffTags": "on",
-                                "color": "#5386E4",
-                                "nameField": "gene",
-                                "searchable": true
-                            },
-                            {
-                                "type": "annotation",
-                                "format": "gff",
-                                "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/tfbs.gff",
-                                "name": "Published TFBS",
-                                "color": "#F5CB5C",
-                                "gffTags": "on",
-                                "nameField": "TF"
-                            },
-                            {
-                                "type": "annotation",
-                                "format": "gff",
-                                "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/tss.gff",
-                                "name": "Published TSS",
-                                "color": "#A6B5AE",
-                                "gffTags": "on",
-                                "nameField": "tss_name"
-                            },
-                            {
-                                "type": "annotation",
-                                "format": "gff",
-                                "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/tu.gff",
-                                "name": "Published TUs",
-                                "color": "#A88132",
-                                "gffTags": "on",
-                                "nameField": "genes"
-                            }
-                        ]
-                    }
-            };
-        igv.createBrowser(igvDiv, options)
-            .then(async function (browser) {
-                console.log("Created IGV browser");
-                var i = row.length - 11
-                while (row[i] != null) {
+    // IGV
+    var igvDiv = document.getElementById("igv-div");
+    var options =
+        {
+            reference:
+                {
+                    "id": "MG1655",
+                    "name": "E. coli MG1655",
+                    "fastaURL": "/ChIPdb/data/e_coli/NC_000913_3/sequence/sequence.fasta",
+                    "indexURL": "/ChIPdb/data/e_coli/NC_000913_3/sequence/sequence.fasta.fai",
+                    "tracks": [
+                        {
+                            "type": "annotation",
+                            "format": "gff",
+                            "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/genes.gff",
+                            "name": "Genes",
+                            "gffTags": "on",
+                            "color": "#0D5899",
+                            "nameField": "gene",
+                            "searchable": true
+                        },
+                        {
+                            "type": "annotation",
+                            "format": "gff",
+                            "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/tu.gff",
+                            "name": "Published TUs",
+                            "color": "#5386E4",
+                            "gffTags": "on",
+                            "nameField": "genes"
+                        },
+                        {
+                            "type": "annotation",
+                            "format": "gff",
+                            "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/tss.gff",
+                            "name": "Published TSS",
+                            "color": "#8E908E",
+                            "gffTags": "on",
+                            "nameField": "tss_name"
+                        },
+                        {
+                            "type": "annotation",
+                            "format": "gff",
+                            "url": "/ChIPdb/data/e_coli/NC_000913_3/annotation/tfbs.gff",
+                            "name": "Published TFBS",
+                            "color": "#F5CB5C",
+                            "gffTags": "on",
+                            "nameField": "TF"
+                        }
+
+                    ]
+                }
+        };
+    igv.createBrowser(igvDiv, options)
+        .then(async function (browser) {
+            console.log("Created IGV browser");
+            var i = row.length - 15;
+            var color_iter = 0;
+            const colors = ['#464746', '#464746', '#9BABA4', '#9BABA4', '#417f5d', '#417f5d', '#817198', '#817198']; // maybe make better someday
+            while (i < row.length) {
+                if (row[i] != null) {
                     await browser.loadTrack({
                         "name": row[i - 1],
                         "type": "wig",
-                        "url": "/ChIPdb/data/e_coli/NC_000913_3/bw/" + row[i]
+                        "url": "/ChIPdb/data/e_coli/NC_000913_3/bw/" + row[i],
+                        "color": colors[color_iter]
                     })
-                    i += 2
                 }
-            })
-
-        // Table
-        $.getJSON('plot_dev/binding_site_table/data/' + TF_name.substr(0, 4) + '_binding_site_table.json', function (data) {
-            // convert rows into objects
-            const container = 'binding_site_table'
-            var tabledata = []
-            for (let i = 0; i < data.length; i++) {
-                var obj = data[i];
-                tabledata.push(obj) //adds items to an object
+                i += 2
+                color_iter += 1
             }
+        })
 
-            for (let i = 0; i < tabledata.length; i++) {
-                const index = tabledata[i].index;
-                const bindCond = tabledata[i].condition;
-                const bindStart = tabledata[i].binding_peak_start;
-                const bindEnd = tabledata[i].binding_peak_end;
-                const peakStrength = tabledata[i].binding_peak_strength;
-                const genes = tabledata[i].target_genes;
-            }
+    // Table
+    $.getJSON('plot_dev/binding_site_table/data/' + TF_name.substr(0, 4) + '_binding_site_table.json', function (data) {
+        // convert rows into objects
+        const container = 'binding_site_table'
+        var tabledata = []
+        for (let i = 0; i < data.length; i++) {
+            var obj = data[i];
+            tabledata.push(obj) //adds items to an object
+        }
 
-            //make a header object for the columns
-            var headerMenu = [
-                {
-                    label: "<i class='fas fa-arrow-right'></i> Move Column to End",
-                    action: function (e, column) {
-                        column.move("end");
-                    }
-                }, {
-                    label: "<i class='fas fa-trash'></i> Hide Column",
-                    action: function (e, column) {
-                        column.hide();
-                    }
+        for (let i = 0; i < tabledata.length; i++) {
+            const index = tabledata[i].index;
+            const bindCond = tabledata[i].condition;
+            const bindStart = tabledata[i].binding_peak_start;
+            const bindEnd = tabledata[i].binding_peak_end;
+            const peakStrength = tabledata[i].binding_peak_strength;
+            const genes = tabledata[i].target_genes;
+        }
+
+        //make a header object for the columns
+        var headerMenu = [
+            {
+                label: "<i class='fas fa-arrow-right'></i> Move Column to End",
+                action: function (e, column) {
+                    column.move("end");
                 }
-            ]
+            }, {
+                label: "<i class='fas fa-trash'></i> Hide Column",
+                action: function (e, column) {
+                    column.hide();
+                }
+            }
+        ]
 
-            // columns object: basic info
-            var columns = [
-                {
-                    title: "TF-#",
-                    field: "index",
-                    sorter: "string",
-                    headerContextMenu: headerMenu
-                },
-                {
-                    title: " Binding Condition",
-                    field: "condition",
-                    sorter: "string",
-                    hozAlign: "left",
-                    formatter: "string",
-                    formatterParams: {precision: 4},
-                    headerContextMenu: headerMenu
-                },
-                {
-                    title: "Start Position",
-                    field: "binding_peak_start",
-                    sorter: "number",
-                    hozAlign: "left",
-                    formatter: "money",
-                    formatterParams: {precision: 0},
-                    headerContextMenu: headerMenu
-                },
-                {
-                    title: "End Position",
-                    field: "binding_peak_end",
-                    sorter: "number",
-                    hozAlign: "left",
-                    formatter: "money",
-                    formatterParams: {precision: 0},
-                    headerContextMenu: headerMenu
-                },
-                {
-                    title: "Peak Intensity",
-                    field: "binding_peak_strength",
-                    sorter: "number",
-                    hozAlign: "left",
-                    formatter: "money",
-                    formatterParams: {precision: 2},
-                    headerContextMenu: headerMenu
-                },
-                {
-                    title: "Target genes",
-                    field: "target_genes",
-                    sorter: "string",
-                    hozAlign: "left",
-                    headerContextMenu: headerMenu
-                },
-            ]
-            var table = new Tabulator("#" + container, {
-                maxHeight: "100%",
-                data: tabledata,
-                columns: columns,
-                layout: "fitDataFill",
-                tooltips: true,
-                movableColumns: true,
-                rowClick: function (e, clicked_row) { //link to the page in a database
-                    var start = clicked_row.getData().binding_peak_start;
-                    var end = clicked_row.getData().binding_peak_end;
-                    igv.removeAllBrowsers()
-                    igv.createBrowser(igvDiv, options)
-                        .then(async function (browser) {
-                            var i = row.length - 11
-                            while (row[i] != null) {
+        // columns object: basic info
+        var columns = [
+            {
+                title: "TF-#",
+                field: "index",
+                sorter: "string",
+                headerContextMenu: headerMenu
+            },
+            {
+                title: " Binding Condition",
+                field: "condition",
+                sorter: "string",
+                hozAlign: "left",
+                formatter: "string",
+                formatterParams: {precision: 4},
+                headerContextMenu: headerMenu
+            },
+            {
+                title: "Start Position",
+                field: "binding_peak_start",
+                sorter: "number",
+                hozAlign: "left",
+                formatter: "money",
+                formatterParams: {precision: 0},
+                headerContextMenu: headerMenu
+            },
+            {
+                title: "End Position",
+                field: "binding_peak_end",
+                sorter: "number",
+                hozAlign: "left",
+                formatter: "money",
+                formatterParams: {precision: 0},
+                headerContextMenu: headerMenu
+            },
+            {
+                title: "Peak Intensity",
+                field: "binding_peak_strength",
+                sorter: "number",
+                hozAlign: "left",
+                formatter: "money",
+                formatterParams: {precision: 2},
+                headerContextMenu: headerMenu
+            },
+            {
+                title: "Target genes",
+                field: "target_genes",
+                sorter: "string",
+                hozAlign: "left",
+                headerContextMenu: headerMenu
+            },
+        ]
+        var table = new Tabulator("#" + container, {
+            maxHeight: "100%",
+            data: tabledata,
+            columns: columns,
+            layout: "fitDataFill",
+            tooltips: true,
+            movableColumns: true,
+            rowClick: function (e, clicked_row) { //link to the page in a database
+                var start = clicked_row.getData().binding_peak_start;
+                var end = clicked_row.getData().binding_peak_end;
+                igv.removeAllBrowsers()
+                igv.createBrowser(igvDiv, options)
+                    .then(async function (browser) {
+                        var i = row.length - 15;
+                        var color_iter = 0;
+                        const colors = ['#464746', '#464746', '#9BABA4', '#9BABA4', '#417f5d', '#417f5d', '#817198', '#817198']; // maybe make better someday
+                        while (i < row.length) {
+                            if (row[i] != null) {
                                 await browser.loadTrack({
                                     "name": row[i - 1],
                                     "type": "wig",
-                                    "color": "#333533",
-                                    "url": "/ChIPdb/data/e_coli/NC_000913_3/bw/" + row[i]
+                                    "url": "/ChIPdb/data/e_coli/NC_000913_3/bw/" + row[i],
+                                    "color": colors[color_iter]
                                 })
-                                i += 2
                             }
-                            browser.search('NC_000913.3:'.concat(numberWithCommas(start - 1000), '-',
-                                numberWithCommas(end + 1000)));
-                        });
-                },
-                tooltips: function (cell) {
-                    return "Click to jump to peak in genome viewer below";
-                }
-            });
-            table.redraw();
-
-            //trigger download of binding site table .csv file
-            document.getElementById("download-table").addEventListener("click", function () {
-                table.download("csv", TF_name + "_binding_site_table.csv");
-            });
-
+                            i += 2
+                            color_iter += 1
+                        }
+                        browser.search('NC_000913.3:'.concat(numberWithCommas(start - 1000), '-',
+                            numberWithCommas(end + 1000)));
+                    });
+            },
+            tooltips: function (cell) {
+                return "Click to jump to peak in genome viewer below";
+            }
         });
+        table.redraw();
+
+        //trigger download of binding site table .csv file
+        document.getElementById("download-table").addEventListener("click", function () {
+            table.download("csv", TF_name + "_binding_site_table.csv");
+        });
+
+    });
+}
+
+// Formatting commas into numbers
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
